5ab8b6c26a0704d3a0dd4cfca0c90e0bd1662eea
Zarrar Khan <zarrar.khan@pnnl.gov>
2021-02-19 13:59:26 -0500

    updating all

---
+++ NEWS.md
@@ -0,0 +1,10 @@
+<!-- ------------------------>
+<!-- ------------------------>
+# exampleRepo 0.1.0 (Under development)
+<p align="center"> <img src="READMEfigs/metisHeaderThick.PNG"></p>
+<!-- ------------------------>
+<!-- ------------------------>
+
+## New features
+
+## Bug Fixes
---
+++ R/data.R
@@ -0,0 +1,21 @@
+#' Example CSV data description.
+#'
+#' @source exampleRepo
+#' @format .csv
+#' @examples
+#' \dontrun{
+#'  library(exampleRepo);
+#'  exampleRepo::exampleDataCSV
+#' }
+"exampleDataCSV"
+
+#' Example R data description.
+#'
+#' @source exampleRepo
+#' @format R
+#' @examples
+#' \dontrun{
+#'  library(exampleRepo);
+#'  exampleRepo::exampleDataR
+#' }
+"exampleDataR"
